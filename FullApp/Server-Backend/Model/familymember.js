706b2dcd92e706adc33aaccdb8a80a12c4bec20a
Dhyan21986 <dhyanpatel453@gmail.com>
2025-07-23 09:53:27 +0530

    Work

--- FullApp/Server-Backend/Model/familymember.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema, model } = mongoose;
-
-const familyMemberSchema = new Schema({
-  relationType: { type: String, required: true },
-  userId:         { type: Schema.Types.ObjectId, ref: 'User', required: true },
-});
-
-export default model('FamilyMember', familyMemberSchema);
